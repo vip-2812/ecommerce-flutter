17f88e55e994e296dbb50a004f1409c15d90902d
rajnis09 <rajnish.kumar2998@gmail.com>
2020-10-04 22:54:01 +0530

    Page to show items in cart #7

---
+++ lib/pages/cart/sample_data.dart
@@ -0,0 +1,48 @@
+class SampleData {
+  final List<Map<String, Object>> shopItems = [
+    {
+      "image": "assets/images/food.jpg",
+      "itemName": "Tea bag",
+      "amount": "250g",
+      "quantity": 1,
+      "price": 150.0,
+    },
+    {
+      "image": "assets/images/food.jpg",
+      "itemName": "Organic Sugar with hand processed",
+      "amount": "1k",
+      "quantity": 1,
+      "price": 60.0,
+    },
+    {
+      "image": "assets/images/food.jpg",
+      "itemName": "Oil ",
+      "amount": "1l",
+      "quantity": 1,
+      "price": 135.0,
+    },
+    {
+      "image": "assets/images/food.jpg",
+      "itemName": "Pulse",
+      "amount": "1k",
+      "quantity": 1,
+      "price": 90.0,
+    },
+    {
+      "image": "assets/images/food.jpg",
+      "itemName": "Atta",
+      "amount": "10k",
+      "quantity": 1,
+      "price": 345.0,
+    },
+    {
+      "image": "assets/images/food.jpg",
+      "itemName": "Rice",
+      "amount": "5k",
+      "quantity": 1,
+      "price": 385.0,
+    }
+  ];
+}
+
+final SampleData sampleData = SampleData();
